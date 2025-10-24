35ca11102f93c448e9cececac31eef21427d3e7b
mohamed keytsane <osamakeysane20@gmail.com>
2025-10-24 08:22:17 +0300

    .

--- alx-rick-and-morty-app/pages/_app.tsx
+++ alx-rick-and-morty-app/pages/_app.tsx
@@ -1,15 +1,15 @@
-import "@/styles/globals.css";
-import type { AppProps } from "next/app";
-import { ApolloProvider } from "@apollo/client/react";
-import client from "@/graphql/apolloClient";
+// import "@/styles/globals.css";
+// import type { AppProps } from "next/app";
+// import { ApolloProvider } from "@apollo/client/react";
+// import client from "@/graphql/apolloClient";
 
-export default function App({ Component, pageProps }: AppProps) {
-  return (
-    <ApolloProvider client={client}>
-      <Component {...pageProps} />
-    </ApolloProvider>
-  );
-}
+// export default function App({ Component, pageProps }: AppProps) {
+//   return (
+//     <ApolloProvider client={client}>
+//       <Component {...pageProps} />
+//     </ApolloProvider>
+//   );
+// }
 /*
 1. import type { AppProps } from "next/app";This line imports a TypeScript type from Next.js.
 AppProps is a type definition that tells TypeScript what props your custom App component receives.
@@ -51,3 +51,15 @@ pageProps → any data or props for that page
 
 
 */
+import ErrorBoundary from "@/components/ErrorBoundary";
+import type { AppProps } from "next/app";
+
+function MyApp({ Component, pageProps }: AppProps) {
+  return (
+    <ErrorBoundary>
+      <Component {...pageProps} />
+    </ErrorBoundary>
+  );
+}
+
+export default MyApp;
