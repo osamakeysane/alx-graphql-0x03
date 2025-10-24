6e015f02e4b8f615b72b2f6b9a2e0d3a3c33d1f8
mohamed keytsane <osamakeysane20@gmail.com>
2025-10-24 08:34:44 +0300

    .

--- alx-rick-and-morty-app/components/ErrorBoundary.tsx
+++ alx-rick-and-morty-app/components/ErrorBoundary.tsx
@@ -1,3 +1,46 @@
+// interface State {
+//   hasError: boolean;
+// }
+
+// interface ErrorBoundaryProps {
+//   children: ReactNode;
+// }
+
+// class ErrorBoundary extends React.Component<ErrorBoundaryProps, State> {
+//   constructor(props: ErrorBoundaryProps) {
+//     super(props);
+//     this.state = { hasError: false };
+//   }
+
+//   static getDerivedStateFromError(error: Error): State {
+//     return { hasError: true };
+//   }
+
+//   componentDidCatch(error: Error, errorInfo: React.ErrorInfo) {
+//     console.log({ error, errorInfo });
+//   }
+
+//   render() {
+//     if (this.state.hasError) {
+//       return (
+//         <div>
+//           <h2>Oops, there is an error!</h2>
+//           <button onClick={() => this.setState({ hasError: false })}>
+//             Try again?
+//           </button>
+//         </div>
+//       );
+//     }
+
+//     return this.props.children;
+//   }
+// }
+
+// export default ErrorBoundary;
+// Updated component with Sentry
+import React, { ReactNode } from "react";
+import * as Sentry from "@sentry/react";
+
 interface State {
   hasError: boolean;
 }
@@ -17,7 +60,11 @@ class ErrorBoundary extends React.Component<ErrorBoundaryProps, State> {
   }
 
   componentDidCatch(error: Error, errorInfo: React.ErrorInfo) {
+    // Log to console
     console.log({ error, errorInfo });
+
+    // Send error to Sentry
+    Sentry.captureException(error, { extra: errorInfo });
   }
 
   render() {
